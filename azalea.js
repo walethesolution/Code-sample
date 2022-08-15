ada7e3a6f8aaf784ad89a1c7cca5bba60aa6f928
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-08-15 13:57:49 -0400

    Add function Majordream

---
+++ azalea.js
@@ -0,0 +1,13 @@
+function Majordream(whatIsIt, actionsNeeded, timeline) {
+  this.what = whatIsIt;
+  this.how = actionsNeeded;
+  this.year = timeline;
+}
+
+const dream1 = new Majordream(
+  "A community of Nigerians",
+  "I keep growing everyday",
+  "when the time is right, it will happen"
+);
+
+console.log(dream1);
